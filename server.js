24890e0b06e6dadf7b660dc67803c01b9ddf1e4d
xiaohua2003 <xcai3@ualberta.ca>
2021-06-19 14:06:53 -0600

    upload final file

---
+++ server.js
@@ -0,0 +1,46 @@
+
+// Require Express to run server and routes
+const express=require("express");
+// Start up an instance of app
+const app=express();
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors=require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port=3000;
+app.listen(port,function(){
+    console.log(`server is working on ${port}`);
+});
+
+let projectData={};
+    
+//POST ROUTE
+app.post("/addWeather",addWeather);
+function addWeather(req, res) {
+    newData={
+        date: req.body.date,
+        temp: req.body.temp,
+        content: req.body.content
+    };
+    projectData=newData;
+    //res.send(projectData);
+    //console.log(projectData);
+};
+
+
+app.get("/all", getData);
+function getData(req, res){
+res.send(projectData);
+console.log(projectData);
+return projectData;
+};
